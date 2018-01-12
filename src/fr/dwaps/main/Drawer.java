cd4988664a794bb6d30b86f244d6c0de26029a12
Michael Cornillon <contact@dwaps.fr>
2018-01-12 15:49:16 +0100

    Draw bordered? animated? rect

--- src/fr/dwaps/main/Drawer.java
+++ src/fr/dwaps/main/Drawer.java
@@ -2,14 +2,37 @@ package fr.dwaps.main;
 
 public class Drawer {
 	private String fill = "O ";
+	private String border = "# ";
+	private boolean animated = false;
+	private boolean bordered = false;
 	
 	public void rect(int width, int height) {
 		for (int i = 0; i < height; i++) {
+			sleep(30);
 			for (int j = 0; j < width; j++) {
-				System.out.print(fill);
+				boolean b = bordered ?
+					(i== 0 || i == height-1 || j == 0 || j == width-1) : false;
+				
+				System.out.print(b ? border : fill);
+				sleep(30);
 			}
 			System.out.println();
 		}
 		System.out.println();
+	}
+	
+	public void sleep(int millis) {
+		if (animated) {
+			try { Thread.sleep(millis); }
+			catch (Exception e) {}
+		}
+	}
+	
+	public void setAnimated(boolean animated) {
+		this.animated = animated;
+	}
+	
+	public void setBordered(boolean bordered) {
+		this.bordered = bordered;
 	}
 }
--- src/fr/dwaps/main/MainClass.java
+++ src/fr/dwaps/main/MainClass.java
@@ -4,6 +4,8 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		Drawer drawer = new Drawer();
+		drawer.setAnimated(false);
+		drawer.setBordered(true);
 		drawer.rect(9,5);
 	}
 
